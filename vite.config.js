fd7c8118b0c9a7ca3acd9d9e1a86a83e7563ba02
bharat-poojari <bharatp0316@gmail.com>
2026-04-15 19:59:04 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -25,6 +26,9 @@ export default defineConfig({
         },
       },
     },
+  },
+  define: {
+    'process.env': {},
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', 'axios', 'zustand'],
