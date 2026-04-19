7abf2255ef2f5b839848187f513a5503ab069abc
bharat-poojari <bharatp0316@gmail.com>
2026-04-19 20:03:09 +0530

    this time for sure

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+// src/pages/index.js
+export { HomePage } from './HomePage';
+export { CategoryPage } from './CategoryPage';
+export { ArticlePage } from './ArticlePage';
+export { SearchPage } from './SearchPage';
+export { TrendingPage } from './TrendingPage';
+export { VideoPage } from './VideoPage';
+export { BookmarksPage } from './BookmarksPage';
+export { ContactPage } from './ContactPage';
